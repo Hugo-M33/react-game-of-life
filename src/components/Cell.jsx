a7282bd853fbfe9fee915f7dd0dacd4bc64b35a2
Hugo Martin <hugo.martin.duloz@gmail.com>
2021-02-02 14:13:34 +0100

    even more styles

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -4,7 +4,7 @@ import {motion} from 'framer-motion'
 
 const Cell = memo(({value, toggleCell, column, row, dispatch}) => {
     return (
-        <motion.div onMouseLeave={e => e.buttons === 1 && dispatch({type: "toggleCell", payload: {column, row}})} whileHover={{ scale: 1.5, boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", borderRadius: "5px" }} onClick={() => dispatch({type: "toggleCell", payload: {column, row}})} style={{ backgroundColor: value ? "#0091AD" : "white"}}>
+        <motion.div onMouseLeave={e => e.buttons === 1 && dispatch({type: "toggleCell", payload: {column, row}})} whileHover={{ scale: 1.5, boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", borderRadius: "5px" }} onMouseUp={() => dispatch({type: "toggleCell", payload: {column, row}})} style={{ backgroundColor: value ? "#0091AD" : "white", cursor: "pointer"}}>
             
         </motion.div>
     )
